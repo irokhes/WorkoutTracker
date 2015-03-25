fa67975d57a3dc09a363717554288f6a3a0097d7
irokhes <jorgereyero@gmail.com>
2015-03-25 22:28:27 +0000

    Initial web api project setup, generic repository

---
+++ WorkoutTracker.Api/Controllers/ExerciseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WorkoutTracker.Api.Controllers
+{
+    public class ExerciseController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Exercise> Get()
+        {
+            return new List<Exercise>
+            {
+                new Exercise{Name = "Bench Press with barebell"},
+                new Exercise{Name = "Bench Press with dumbell"}
+            };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Exercise
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ WorkoutTracker.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkoutTracker.Api.WebApiApplication" Language="C#" %>
