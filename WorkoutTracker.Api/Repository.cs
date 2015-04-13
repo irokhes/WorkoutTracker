a02c5c352e4fdd6420f5c7b1e1254c604f6d9ecf
irokhes <jorgereyero@gmail.com>
2015-04-13 21:04:43 +0100

    Add unit of work and repository implementation

---
+++ WorkoutTracker.Api/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WorkoutTracker.Api
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        readonly DbContext _context;
+        DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> filter)
+        {
+            return _dbSet.Where(filter);
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Delete(int id)
+        {
+            T entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+    }
+}
---
+++ WorkoutTracker.Api/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WorkoutTracker.Api
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> RepositoryFor<T>() where T : class;
+        void Commit();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            _context = context;
+        }
+
+        public IRepository<T> RepositoryFor<T>() where T : class
+        {
+            return new Repository<T>(_context);
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
