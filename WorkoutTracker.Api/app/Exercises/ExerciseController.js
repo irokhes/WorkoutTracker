c44c6dd1bf48338ccb15fea1af467e385ee5fa1b
irokhes <jorgereyero@gmail.com>
2015-06-02 21:54:15 +0100

    add exercises filter

--- WorkoutTracker.Api/app/Exercises/ExerciseController.js
+++ WorkoutTracker.Api/app/Exercises/ExerciseController.js
@@ -1,23 +1,47 @@
 ﻿(function() {
     'use strict';
-    app.controller('ExerciseController', ['$scope','exerciseService', function ($scope, exerciseService) {
+    app.controller('ExerciseController', ['$scope', 'exerciseService', '$filter', function ($scope, exerciseService, $filter) {
         $scope.exercices = [];
         $scope.totalExercises = 0;
-        $scope.categories = ['All','Biceps', 'Triceps', 'Back', 'Chest', 'Legs', 'Abs'];
-        $scope.categories.selected = $scope.categories[0];
+        $scope.filteredExercises = [];
+        $scope.totalFilteredExercises = 0;
+        $scope.muscularGroup = ['All','Biceps', 'Triceps', 'Back', 'Chest', 'Legs', 'Abs'];
+        $scope.selectedMuscularGroup = $scope.muscularGroup[0];
+        $scope.filterValue = '';
+
         init();
 
         function init() {
-            exerciseService.get().success(function(exercises) {
+            createWatche();
+            getExercises();
+        };
+
+        function getExercises() {
+            exerciseService.get().success(function (exercises) {
                 $scope.exercices = exercises;
                 $scope.totalExercises = $scope.exercices.length;
-            })
+                filterExercises('', 'All');
+                })
             .error(function (error) {
                 $scope.status = 'Unable to load exercises: ' + error.message;
             });
+        }
+
+        function filterExercises(filterValue, muscularGroup) {
+            $scope.filteredExercises = $filter('exercisesFilter')($scope.exercices, filterValue, muscularGroup);
+            $scope.totalFilteredExercises = $scope.filteredExercises.length;
         };
 
+        function createWatche() {
 
+            $scope.$watch("filterValue", function (filterValue) {
+                filterExercises(filterValue, $scope.selectedMuscularGroup);
+            });
 
-        }]);
+            $scope.$watch("selectedMuscularGroup", function (selectedMuscularGroup) {
+                filterExercises($scope.filterValue, selectedMuscularGroup);
+            });
+        }
+
+    }]);
 })();
---
+++ WorkoutTracker.Api/app/Filters/exercisesFilter.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    app.filter('exercisesFilter', function() {
+        return function (exercises, filterValue, muscularGroup) {
+            if (!filterValue && muscularGroup === 'All')
+                return exercises;
+            var matches = [];
+
+            for (var i = 0; i < exercises.length; i++) {
+                var isTheRightMuscularGroup = muscularGroup === 'All' || exercises[i].MuscularGroup === muscularGroup;
+                var containsSearchTerms = exercises[i].Name.toLowerCase().indexOf(filterValue.toLowerCase()) > -1;
+
+                if (containsSearchTerms && isTheRightMuscularGroup) {
+                    matches.push(exercises[i]);
+
+                }
+            }
+            return matches;
+        };
+    });
+})();
